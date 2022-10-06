9a0bdad272e349269e8c05bcae943cbdd61ebb4b
alencarrkevin <110054825+alencarrkevin@users.noreply.github.com>
2022-10-06 11:58:37 -0300

    site empresa

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+document.querySelector('#menu-btn').addEventListener('click', function(){
+  //  console.log('menu');
+
+    document.querySelector('#menu-site').classList.toggle("active");
+
+    document.querySelector('#menu-icon').classList.toggle("active")
+
+})
